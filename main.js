2f115719634700f1145a90ffb8ad786763f3bec4
Matthew <mf9646a@alumni.american.edu>
2019-05-22 13:34:34 -0400

    completed all objectives

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById("modal").className = "hidden";
+})
 
+let likeButton = document.querySelectorAll("#like")
+      likeButton.forEach((button) => {
+        button.addEventListener('click', function() {
+        mimicServerCall().then(function(a) {
+          if (!button.classList.contains("activated-heart")) {
+            button.innerText = FULL_HEART
+            button.classList.add("activated-heart")
+          } else if (button.classList.contains("activated-heart")) {
+            button.innerText = EMPTY_HEART
+            button.classList.remove("activated-heart")
+          }
+        }).catch(function(b) {
+          document.getElementById("modal").innerText = `${b}`
+          document.getElementById("modal").classList.remove("hidden")
+          setTimeout(function() {
+            document.getElementById("modal").classList.add("hidden")
+          }, 5000);
+        })
+      })
+    })
 
+//Keep all your styling rules entirely in `style.css`. Do not manipulate any `.style` properties.
+//Only manipulate the DOM once the server requests respond. Do not make the heart full until you're inside a successful `.then` block.
 
-
-//------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
